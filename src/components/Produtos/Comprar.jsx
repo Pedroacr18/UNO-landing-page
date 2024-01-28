dede30e0637efa68c8aac0de3586a90662a012ec
Pedro Arthur <parthurcruzribeiro17@gmail.com>
2024-01-28 16:43:00 -0300

    partes do css da pagina produtos feito

--- src/components/Produtos/Comprar.jsx
+++ src/components/Produtos/Comprar.jsx
@@ -9,7 +9,7 @@ function Comprar() {
                 <div className={styles.comprar_img}>
                     <img src={uno_min} alt="uno minimalista"></img>
                 </div>
-
+                
                 <div className={styles.comprar_content}>
 
                 <div>
@@ -17,14 +17,14 @@ function Comprar() {
                 </div>
 
                 <div className={styles.comprar_preco}>
-                    <h2>R$ 80,00</h2>
-                    <p>Acaba em <span>01.34.45</span></p>
+                    <h2 className={styles.comprar_h2}>R$ 80,00</h2>
+                    <p className={styles.comprar_p}>Acaba em <span className={styles.comprar_span}>01.34.45</span></p>
                 </div>
 
                 <div>
-                    <p>Sobre este item:</p>
+                    <p className={styles.comprar_sobre}>Sobre este item:</p>
                     <div>
-                    <ul>
+                    <ul className={styles.comprar_lista}>
                         <li>Edição especial - visual completamente único</li>
                         <li>Minimalista - projetado com uma estética minimalista.</li>
                         <li>Visual novo <br /> - visual totalmente novo, bonito e simplista idealizado pelo designer warleson oliveira</li>
@@ -36,8 +36,8 @@ function Comprar() {
                 </div>
 
                 <div className={styles.comprar_menu}>
-                    <h2>R$ 80,00</h2>
-                    <p>Entrega GRÁTIS: Segunda-feira, 29 de Janeiro. Se pedir dentro de 2 hrs 2 mins</p>
+                    <h2 className={styles.comprar_h2}>R$ 80,00</h2>
+                    <p>Entrega GRÁTIS: <span className={styles.comprar_span}>Segunda-<br />feira, 29 de Janeiro.</span> Se<br /> pedir dentro de<span className={styles.comprar_span2}> 2 hrs 2 mins</span></p>
                     <p>Em estoque</p>
                     <button className={styles.comprar_btn} type="submit">Comprar</button>
                     <div>
